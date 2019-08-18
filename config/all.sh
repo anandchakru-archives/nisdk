c7508fe2fdc307df710e76f4af5a87b1a87f9eeb
Anand C <anandchakru.forum@gmail.com>
2019-08-18 15:02:21 -0700

    npm publish nelem

--- config/all.sh
+++ config/all.sh
@@ -13,7 +13,7 @@ git status && git add --all && git status && git commit --message "$commitMsg"
 # patch the version (this also creates a new tag)
 npm version patch && (cd ./projects/nlib; npm version patch) && (cd ./projects/nelem; npm version patch)
 # update buildinfo with new version, build nelem & nlib & publish nlib (to npmjs)
-npm run buildinfo && npm run build:elem && npm run publish:lib && npm run publish:elem
+npm run buildinfo && npm run build:elem && npm run publish:lib && cp ./projects/nelem/package.json ./dist/nelem/ && cp ./projects/nelem/README.md ./dist/nelem/ && npm run publish:elem
 # push tags to github
 git push && git push --tags
 # publish nelem (to github-releases)
