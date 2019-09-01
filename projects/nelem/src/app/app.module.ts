45b91d46983b744ce6baa6b04559515918987027
Anand C <anandchakru.forum@gmail.com>
2019-08-31 23:41:29 -0700

    exported nelem components

--- projects/nelem/src/app/app.module.ts
+++ projects/nelem/src/app/app.module.ts
@@ -3,7 +3,28 @@ import { NgModule, Injector } from '@angular/core';
 import { createCustomElement, WithProperties, NgElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
-import { NlibModule } from 'projects/nlib/src/public-api';
+import {
+  NlibModule, RDefaultComponent,
+  RGuestComponent,
+  RBgComponent,
+  RMainComponent,
+  RMainTitleComponent,
+  RMainShortMsgComponent,
+  RMainImgComponent,
+  RMainLongMsgComponent,
+  RSliderComponent,
+  RDetailsComponent,
+  RDetailsAddrComponent,
+  RDetailsAddrTitleComponent,
+  RDetailsAddrTextComponent,
+  RDetailsTimeFromComponent,
+  RDetailsTimeFromTitleComponent,
+  RDetailsTimeFromTextComponent,
+  RDetailsTimeToComponent,
+  RDetailsTimeToTitleComponent,
+  RDetailsTimeToTextComponent,
+  RDetailsTimeComponent
+} from 'projects/nlib/src/public-api';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -28,6 +49,69 @@ export class AppModule {
   ngDoBootstrap() {
     if (!customElements.get('nelem-nivite')) {
       customElements.define('nelem-nivite', createCustomElement(AppComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-default')) {
+      customElements.define('nelem-r-default', createCustomElement(RDefaultComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-bg')) {
+      customElements.define('nelem-r-bg', createCustomElement(RBgComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details')) {
+      customElements.define('nelem-r-details', createCustomElement(RDetailsComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details-addr')) {
+      customElements.define('nelem-r-details-addr', createCustomElement(RDetailsAddrComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details-addr-text')) {
+      customElements.define('nelem-r-details-addr-text', createCustomElement(RDetailsAddrTextComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details-addr-title')) {
+      customElements.define('nelem-r-details-addr-title', createCustomElement(RDetailsAddrTitleComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details-time')) {
+      customElements.define('nelem-r-details-time', createCustomElement(RDetailsTimeComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details-time-from')) {
+      customElements.define('nelem-r-details-time-from', createCustomElement(RDetailsTimeFromComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details-time-from-text')) {
+      customElements.define('nelem-r-details-time-from-text', createCustomElement(RDetailsTimeFromTextComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details-time-from-title')) {
+      customElements.define('nelem-r-details-time-from-title', createCustomElement(RDetailsTimeFromTitleComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details-time-to')) {
+      customElements.define('nelem-r-details-time-to', createCustomElement(RDetailsTimeToComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details-time-to-text')) {
+      customElements.define('nelem-r-details-time-to-text', createCustomElement(RDetailsTimeToTextComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-details-time-to-title')) {
+      customElements.define('nelem-r-details-time-to-title', createCustomElement(RDetailsTimeToTitleComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-guest')) {
+      customElements.define('nelem-r-guest', createCustomElement(RGuestComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-main')) {
+      customElements.define('nelem-r-main', createCustomElement(RMainComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-main-img')) {
+      customElements.define('nelem-r-main-img', createCustomElement(RMainImgComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-main-long-msg')) {
+      customElements.define('nelem-r-main-long-msg', createCustomElement(RMainLongMsgComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-main-short-msg')) {
+      customElements.define('nelem-r-main-short-msg', createCustomElement(RMainShortMsgComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-main-title')) {
+      customElements.define('nelem-r-main-title', createCustomElement(RMainTitleComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-slider')) {
+      customElements.define('nelem-r-slider', createCustomElement(RSliderComponent, { injector: this.injector }));
+    }
+    if (!customElements.get('nelem-r-default')) {
+      customElements.define('nelem-r-default', createCustomElement(RDefaultComponent, { injector: this.injector }));
     }
   }
 }
--- projects/nlib/src/public-api.ts
+++ projects/nlib/src/public-api.ts
@@ -6,3 +6,25 @@ export * from './lib/nlib.service';
 export * from './lib/nlib.component';
 export * from './lib/nlib.module';
 export * from './lib/services/clog.service';
+
+// Render Components
+export * from './lib/components/r-bg/r-bg.component';
+export * from './lib/components/r-default/r-default.component';
+export * from './lib/components/r-details/r-details.component';
+export * from './lib/components/r-details-addr/r-details-addr.component';
+export * from './lib/components/r-details-addr-text/r-details-addr-text.component';
+export * from './lib/components/r-details-addr-title/r-details-addr-title.component';
+export * from './lib/components/r-details-time/r-details-time.component';
+export * from './lib/components/r-details-time-from/r-details-time-from.component';
+export * from './lib/components/r-details-time-from-text/r-details-time-from-text.component';
+export * from './lib/components/r-details-time-from-title/r-details-time-from-title.component';
+export * from './lib/components/r-details-time-to/r-details-time-to.component';
+export * from './lib/components/r-details-time-to-text/r-details-time-to-text.component';
+export * from './lib/components/r-details-time-to-title/r-details-time-to-title.component';
+export * from './lib/components/r-guest/r-guest.component';
+export * from './lib/components/r-main/r-main.component';
+export * from './lib/components/r-main-img/r-main-img.component';
+export * from './lib/components/r-main-long-msg/r-main-long-msg.component';
+export * from './lib/components/r-main-short-msg/r-main-short-msg.component';
+export * from './lib/components/r-main-title/r-main-title.component';
+export * from './lib/components/r-slider/r-slider.component';
